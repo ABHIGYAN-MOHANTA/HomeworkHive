74c2e2ce8124a46f95319b8104de9c969af7f000
Abhigyan Mohanta <110360901+ABHIGYAN-MOHANTA@users.noreply.github.com>
2024-02-02 23:51:32 +0530

    fake attendance

--- src/app/services/fake-attendance/page.tsx
+++ src/app/services/fake-attendance/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Navbar from "@/components/navbar";
+import Timeline from "@/components/timeline";
+import Cards from "@/components/cards";
 
 export default function page() {
   return (
@@ -7,9 +9,49 @@ export default function page() {
       className="flex min-h-screen bg-center bg-cover flex-col items-center justify-between p-10"
       style={{ backgroundImage: "url('background/background5.jpg')" }}
     >
+      {/* Dark transparent blur overlay */}
+      <div className="absolute inset-0 bg-black bg-opacity-50 blur"></div>
       <Navbar />
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-14 font-sans">
-        <h1>Fake Attendance</h1>
+      <div className="flex flex-col lg:flex-row items-center justify-center mt-14 font-sans z-10">
+        <div className="lg:w-1/3 lg:mr-8">
+          <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-center text-white mb-8">
+            Fake Attendance
+          </h1>
+          <Timeline
+            title1="Choose an Homework Hive Expert"
+            title2="Choose a week of fake attendance"
+            title3="The Expert accepts your request based on fully transparent negotiations"
+            title4="The service is delivered to you"
+            title5="You pay cash on delivery"
+          />
+        </div>
+        <div className="lg:w-2/3 lg:ml-8 flex flex-wrap justify-center ">
+          <Cards
+            title="Naruto Uzumaki - ₹500"
+            badge="New"
+            desc="Ever been really busy with personal stuff, or out on a vacation, or just too lazy to attend classes?"
+            tag1="Hot"
+            tag2="Trending"
+            pic="pic2.webp"
+          />
+          <Cards
+            title="Monkey D Luffy - ₹500"
+            badge="New"
+            desc="Ever been really busy with personal stuff, or out on a vacation, or just too lazy to attend classes?"
+            tag1="Hot"
+            tag2="Trending"
+            pic="pic2.webp"
+          />
+          <Cards
+            title="Ichigo Kurasaki - ₹500"
+            badge="New"
+            desc="Ever been really busy with personal stuff, or out on a vacation, or just too lazy to attend classes?"
+            tag1="Hot"
+            tag2="Trending"
+            pic="pic2.webp"
+          />
+          {/* Add more Cards components here */}
+        </div>
       </div>
     </main>
   );
